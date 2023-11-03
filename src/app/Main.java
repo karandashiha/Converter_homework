f9033296b50343033d1da906d3f76265a2bb88aa
Katy Reshetnyk <kreshetnyk@gmail.com>
2023-11-04 00:02:44 +0200

    Add conversion of kilometers to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,13 +5,21 @@ public class Main {
 
         double miles = 5.0;
         double km = milesToKm(miles);
-        System.out.println( miles + " miles = " + km + " km");
+        System.out.println(miles + " miles = " + km + " km.");
+
+        double km1 = 25.0;
+        double miles1 = kmToMiles(km);
+        System.out.println(km1 + " kilometers = " + miles1 + " miles.");
+
+
     }
-    public static double milesToKm(double miles){
+
+    public static double milesToKm(double miles) {
         // 1 кілометр = 0.621371 миль
         return miles * 1.60934;
     }
-    public static  double kmToMiles(double km){
+
+    public static double kmToMiles(double km) {
         // 1 миля = 1.60934 кілометра
         return km * 0.621371;
     }
